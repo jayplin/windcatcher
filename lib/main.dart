ab5ef5e66ce71874bb296de2aec49a1c61e04922
Jannik Schröer <jannik.schroeer@sap.com>
2022-09-04 19:39:42 +0200

    add to platform widget work

--- lib/main.dart
+++ lib/main.dart
@@ -11,58 +11,12 @@ void main() {
   runApp(MyApp());
 }
 
-// class nMyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Windcatcher',
-//       home: TodosPage(),
-//     );
-//   }
-// }
-
-// void nmain() {
-//   runApp(nMyApp());
-// }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final materialTheme = ThemeData(
-      cupertinoOverrideTheme: const CupertinoThemeData(
-        primaryColor: Color(0xff127EFB),
-      ),
-      primarySwatch: Colors.green,
-      outlinedButtonTheme: OutlinedButtonThemeData(
-        style: ButtonStyle(
-          padding: MaterialStateProperty.all(const EdgeInsets.all(16.0)),
-          foregroundColor: MaterialStateProperty.all(const Color(0xFF3DDC84)),
-        ),
-      ),
-    );
-
-    return Theme(
-      data: materialTheme,
-      child: PlatformProvider(
-        settings: PlatformSettingsData(iosUsesMaterialWidgets: true),
-        builder: (context) => PlatformApp(
-          localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
-            DefaultMaterialLocalizations.delegate,
-            DefaultWidgetsLocalizations.delegate,
-            DefaultCupertinoLocalizations.delegate,
-          ],
-          title: 'Flutter Platform Widgets',
-          home: TodosPage(),
-          material: (_, __) => MaterialAppData(
-            theme: materialTheme,
-          ),
-          cupertino: (_, __) => CupertinoAppData(
-            theme: const CupertinoThemeData(
-              primaryColor: Color(0xff127EFB),
-            ),
-          ),
-        ),
-      ),
+    return PlatformApp(
+      title: 'R',
+      home: TodosPage(),
     );
   }
 }
