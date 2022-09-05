536e7e40cbe058f8e51e9fbb2ff4cdbb58e43871
Jannik Schröer <jannik.schroeer@sap.com>
2022-09-05 10:00:02 +0200

    use const constructor

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,12 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'pages/TodosPage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return PlatformApp(
